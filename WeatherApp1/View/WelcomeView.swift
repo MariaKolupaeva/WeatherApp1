9f2d2decd43432189e7bf756e37e57c3f94434d8
MariaKolupaeva <84396033+MariaKolupaeva@users.noreply.github.com>
2022-05-31 15:01:41 +0300

    Add files via upload

---
+++ WeatherApp1/View/WelcomeView.swift
@@ -0,0 +1,42 @@
+//
+//  WelcomeView.swift
+//  WeatherApp1
+//
+//  Created by Мария Колупаева on 27.05.2022.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("Добро пожаловать 👋🏼")
+                    .bold().font(.title)
+                
+                Text("Пожалуйста, укажите Ваше местоположение")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+            
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
---
+++ WeatherApp1/WeatherApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp1App.swift
+//  WeatherApp1
+//
+//  Created by Мария Колупаева on 27.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
